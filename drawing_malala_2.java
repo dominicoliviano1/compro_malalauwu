d8ae2d8882fd05f9fb913cd467259864ad2c6c4c
dominicoliviano1 <145301799+dominicoliviano1@users.noreply.github.com>
2023-09-18 07:58:45 +0800

    Add files via upload

---
+++ drawing_malala_2.java
@@ -0,0 +1,25 @@
+class drawing_malala_2 {
+    public static void main(String[] args) {
+        System.out.println("                                        ^^");
+        System.out.println("   ^^      ..                                       ..");
+        System.out.println("           []                                       []");
+        System.out.println("         .:[]:_          ^^                       ,:[]:.");
+        System.out.println("       .: :[]: :-.                             ,-: :[]: :.");
+        System.out.println("     .: : :[]: : :`._                       ,.': : :[]: : :.");
+        System.out.println("   .: : : :[]: : : : :-._               _,-: : : : :[]: : : :.");
+        System.out.println("_..: : : : :[]: : : : : : :-._________.-: : : : : : :[]: : : : :-._");
+        System.out.println("_:_:_:_:_:_:[]:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:[]:_:_:_:_:_:_");
+        System.out.println("!!!!!!!!!!!![]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![]!!!!!!!!!!!!!");
+        System.out.println("^^^^^^^^^^^^[]^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[]^^^^^^^^^^^^^");
+        System.out.println("            []                                       []");
+        System.out.println("            []                                       []");
+        System.out.println("            []                                       []");
+        System.out.println(" ~~^-~^_~^~l  l~^-~^~_~^-~_^~-^~_^~~-^~_~^~-~_~-^~_^l  l~^-~_~^-~~-");
+        System.out.println("~ _~~- ~^-^~-^~~- ^~_^-^~~_ -~^_ -~_-~~^- _~~_~-^_ ~^-^~~-_^-~ ~^");
+        System.out.println("   ~ ^- _~~_-  ~~ _ ~  ^~  - ~~^ _ -  ^~-  ~ _  ~~^  - ~_   - ~^_~");
+        System.out.println("     ~-  ^_  ~^ -  ^~ _ - ~^~ _   _~^~-  _ ~~^ - _ ~ - _ ~~^ -");
+        System.out.println("jgs     ~^ -_ ~^^ -_ ~ _ - _ ~^~-  _~ -_   ~- _ ~^ _ -  ~ ^-");
+        System.out.println("            ~^~ - _ ^ - ~~~ _ - _ ~-^ ~ __- ~_ - ~  ~^_-");
+        System.out.println("                ~ ~- ^~ -  ~^ -  ~ ^~ - ~~  ^~ - ~");
+    }
+}

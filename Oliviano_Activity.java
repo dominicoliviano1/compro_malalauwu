5a864b4132f7c40f0932dc99ea397c56ba482988
dominicoliviano1 <145301799+dominicoliviano1@users.noreply.github.com>
2023-10-16 08:09:00 +0800

    Add files via upload

---
+++ Oliviano_Activity.java
@@ -0,0 +1,32 @@
+package oliviano_activity;
+
+public class Oliviano_Activity  {
+    public static void main(String [] args) {
+    
+        int hihi = 90;
+        int hehe = 900;
+        float total = 8;
+        
+       
+        
+        float add = hihi + hehe;
+        System.out.println("Addition:       " + add);
+        
+        float sub = hihi - hehe;
+        System.out.println("Subtraction:       " + sub);
+        
+        float multi = hihi * hehe;
+        System.out.println("Multiplication:       " + multi);
+        
+        float div = hehe / hihi;
+        System.out.println("Division:       " + div);
+        
+        float per = hihi * total;
+        System.out.println("Percentage:    " + per + "%");
+        
+        System.out.println ("Subtraction Increment:    " + --hihi);
+        System.out.println ("Addition Increment:    " + ++hehe);
+        
+    }
+    
+}
